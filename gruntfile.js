b7b6d855c769c9e2a56f6bb8d5afec1f8dfa1d5c
sergi galceran <sergi galceran>
2017-03-06 11:34:14 +0100

    funciones grunt

--- gruntfile.js
+++ gruntfile.js
@@ -15,18 +15,39 @@ module.exports = function(grunt) {
           './angular/*.js'
         ],
         entry: 'core.js',
-        out: 'angular/obfuscated.js',
+        out: 'dist/core.myapp.min.js',
         strings: true,
         root: __dirname
-      }
+      },
+      watch: {
+  scripts: {
+    files: ['angular/*.js'],
+    tasks: ['jshint'],
+    options: {
+      spawn: false,
+    },
+  },
+},
+concat: {
+    options: {
+      separator: ';',
+    },
+    dist: {
+      src: ['angular/core.js', 'angular/utils.js'],
+      dest: 'dist/built.js',
+    },
+  }
     });
 
  // Load the plugin that provides the "uglify" task.
  grunt.loadNpmTasks('grunt-contrib-uglify');
  grunt.loadNpmTasks('grunt-obfuscator');
+ grunt.loadNpmTasks('grunt-contrib-concat');
+ grunt.loadNpmTasks('grunt-contrib-watch');
 
  // Default task(s).
  grunt.registerTask('default', ['uglify']);
- grunt.registerTask('ofuscar', ['obfuscator']);
+ grunt.registerTask('ofuscar', ['concat','obfuscator']);
+
 
 };
